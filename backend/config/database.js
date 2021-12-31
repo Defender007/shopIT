5262c361fcf51620272473fc1caeaeb14741f4b3
Defender007 <mydjangopythonprojects@gmail.com>
2021-12-31 13:39:42 +0100

    chore: handles uncaughException...but I have to work arround the exception in database connection catch block

--- backend/config/database.js
+++ backend/config/database.js
@@ -11,7 +11,12 @@ const connectDatabase = () => {
       console.log(
         `mongoDB database connected with HOST: ${con.connection.host}`
       );
-    }).catch(err => console.log("Could not connect to MongoDB...", err));
+    }).catch(err => {
+      console.log("Could not connect to MongoDB...", err.message)
+      //... Handle Unhandled Promise rejections here...
+      //...as against the tutorial module5-5
+      process.exit(1);
+    });
 };
 
 module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,17 @@ dotenv.config({path: 'backend/config/config.env'})
 // connecting to database...
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+})
+
+//... Handle Unhandled Promise rejections...
+// ...this did not work as described in tutorial
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.message}`);
+    console.log('Shutting down the Server due to Unhandled Promise rejections');
+    server.close((err) => {
+        console.log(`ERROR: ${err.message}`);
+        process.exit(1)
+    });
 })
