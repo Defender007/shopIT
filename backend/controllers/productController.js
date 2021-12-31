e62676dde4ace1793930d8c099b578f21f83d16d
Defender007 <mydjangopythonprojects@gmail.com>
2021-12-31 11:03:47 +0100

    feature: wraps all route handlers function with catchAsyncErrors in order to catch any error that occurs in any asyncronous calls...adds error middleware in route calls...

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,20 +1,19 @@
-const mongoose = require("mongoose");
 const Product = require("../models/product");
-
 const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 
-// create a new product => /api/vi/product/new
-exports.newProduct = async (req, res, next) => {
+// create a new product => /api/vi/admin/product/new
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
 
   res.status(201).json({
     product,
     success: true,
   });
-};
+});
 
 // Get all products => /api/v1/products
-exports.getProducts = async (req, res, next) => {
+exports.getProducts = catchAsyncErrors(async (req, res, next) => {
   const products = await Product.find();
 
   res.status(200).json({
@@ -22,10 +21,10 @@ exports.getProducts = async (req, res, next) => {
     count: products.length,
     products,
   });
-};
+});
 
 // Get single product details => /api/v1/product/:id
-exports.getSingleProduct = async (req, res, next) => {
+exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
   const mongooseValidType = mongoose.Types.ObjectId.isValid(req.params.id);
   console.log("mongoose error: ", mongooseValidType);
 
@@ -42,22 +41,19 @@ exports.getSingleProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 // Update product  => /api/v1/admin/product/:id
-exports.updateProduct = async (req, res, next) => {
-    const mongooseValidType = mongoose.Types.ObjectId.isValid(req.params.id);
-    console.log("mongoose error: ", mongooseValidType);
-  
-    let product = mongooseValidType
-      ? await Product.findById(req.params.id)
-      : mongooseValidType;
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+  const mongooseValidType = mongoose.Types.ObjectId.isValid(req.params.id);
+  console.log("mongoose error: ", mongooseValidType);
+
+  let product = mongooseValidType
+    ? await Product.findById(req.params.id)
+    : mongooseValidType;
 
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -69,17 +65,14 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+})
 
 //... delete product  => /api/v1/admin/product/:id
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
   Product.remove();
 
@@ -87,4 +80,4 @@ exports.deleteProduct = async (req, res, next) => {
     success: true,
     message: "Product was deleted!",
   });
-};
+});
